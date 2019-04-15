5146d45c0151302b57f88f242745c409cccc1e39
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-04-15 17:01:20 -0300

    comment out

--- modules/fflch_fakecontent/src/InstallHelper.php
+++ modules/fflch_fakecontent/src/InstallHelper.php
@@ -14,7 +14,7 @@ use Drupal\Component\Utility\Html;
  * Defines a helper class for importing default content.
  *
  * @internal
- *   This code is only for use by the Umami demo: Content module.
+ *   This code is only for use to import Content.
  */
 class InstallHelper implements ContainerInjectionInterface {
 
@@ -81,201 +81,9 @@ class InstallHelper implements ContainerInjectionInterface {
    * Imports default contents.
    */
   public function importContent() {
-    $this->importEditors()
-      ->importArticles()
-      ->importRecipes()
+    $this->
       ->importPages()
       ->importBlockContent();
-  }
-
-  /**
-   * Imports editors.
-   *
-   * Other users are created as their content is imported. However, editors
-   * don't have their own content so are created here instead.
-   *
-   * @return $this
-   */
-  protected function importEditors() {
-    $user_storage = $this->entityTypeManager->getStorage('user');
-    $editors = [
-      'Margaret Hopper',
-      'Grace Hamilton',
-    ];
-    foreach ($editors as $name) {
-      $user = $user_storage->create([
-        'name' => $name,
-        'status' => 1,
-        'roles' => ['editor'],
-        'mail' => mb_strtolower(str_replace(' ', '.', $name)) . '@example.com',
-      ]);
-      $user->enforceIsNew();
-      $user->save();
-      $this->storeCreatedContentUuids([$user->uuid() => 'user']);
-    }
-    return $this;
-  }
-
-  /**
-   * Imports articles.
-   *
-   * @return $this
-   */
-  protected function importArticles() {
-    $module_path = $this->moduleHandler->getModule('fflch_fakecontent')
-      ->getPath();
-    if (($handle = fopen($module_path . '/default_content/articles.csv', "r")) !== FALSE) {
-      $uuids = [];
-      $header = fgetcsv($handle);
-      while (($data = fgetcsv($handle)) !== FALSE) {
-        $data = array_combine($header, $data);
-        // Prepare content.
-        $values = [
-          'type' => 'article',
-          'title' => $data['title'],
-          'moderation_state' => 'published',
-        ];
-        // Fields mapping starts.
-        // Set Body Field.
-        if (!empty($data['body'])) {
-          $body_path = $module_path . '/default_content/article_body/' . $data['body'];
-          $body = file_get_contents($body_path);
-          if ($body !== FALSE) {
-            $values['body'] = [['value' => $body, 'format' => 'basic_html']];
-          }
-        }
-        // Set node alias if exists.
-        if (!empty($data['slug'])) {
-          $values['path'] = [['alias' => '/' . $data['slug']]];
-        }
-        // Set field_tags if exists.
-        if (!empty($data['tags'])) {
-          $values['field_tags'] = [];
-          $tags = explode(',', $data['tags']);
-          foreach ($tags as $term) {
-            $values['field_tags'][] = ['target_id' => $this->getTerm($term)];
-          }
-        }
-        // Set article author.
-        if (!empty($data['author'])) {
-          $values['uid'] = $this->getUser($data['author']);
-        }
-        // Set Image field.
-        if (!empty($data['image'])) {
-          $path = $module_path . '/default_content/images/' . $data['image'];
-          $values['field_image'] = [
-            'target_id' => $this->createFileEntity($path),
-            'alt' => $data['alt'],
-          ];
-        }
-
-        // Create Node.
-        $node = $this->entityTypeManager->getStorage('node')->create($values);
-        $node->save();
-        $uuids[$node->uuid()] = 'node';
-      }
-      $this->storeCreatedContentUuids($uuids);
-      fclose($handle);
-    }
-    return $this;
-  }
-
-  /**
-   * Imports recipes.
-   *
-   * @return $this
-   */
-  protected function importRecipes() {
-    $module_path = $this->moduleHandler->getModule('fflch_fakecontent')->getPath();
-
-    if (($handle = fopen($module_path . '/default_content/recipes.csv', "r")) !== FALSE) {
-      $header = fgetcsv($handle);
-      $uuids = [];
-      while (($data = fgetcsv($handle)) !== FALSE) {
-        $data = array_combine($header, $data);
-        $values = [
-          'type' => 'recipe',
-          // Title field.
-          'title' => $data['title'],
-          'moderation_state' => 'published',
-        ];
-        // Set article author.
-        if (!empty($data['author'])) {
-          $values['uid'] = $this->getUser($data['author']);
-        }
-        // Set node alias if exists.
-        if (!empty($data['slug'])) {
-          $values['path'] = [['alias' => '/' . $data['slug']]];
-        }
-        // Set field_image field.
-        if (!empty($data['image'])) {
-          $image_path = $module_path . '/default_content/images/' . $data['image'];
-          $values['field_image'] = [
-            'target_id' => $this->createFileEntity($image_path),
-            'alt' => $data['alt'],
-          ];
-        }
-        // Set field_summary Field.
-        if (!empty($data['summary'])) {
-          $values['field_summary'] = [['value' => $data['summary'], 'format' => 'basic_html']];
-        }
-        // Set field_recipe_category if exists.
-        if (!empty($data['recipe_category'])) {
-          $values['field_recipe_category'] = [];
-          $tags = array_filter(explode(',', $data['recipe_category']));
-          foreach ($tags as $term) {
-            $values['field_recipe_category'][] = ['target_id' => $this->getTerm($term, 'recipe_category')];
-          }
-        }
-        // Set field_preparation_time Field.
-        if (!empty($data['preparation_time'])) {
-          $values['field_preparation_time'] = [['value' => $data['preparation_time']]];
-        }
-        // Set field_cooking_time Field.
-        if (!empty($data['cooking_time'])) {
-          $values['field_cooking_time'] = [['value' => $data['cooking_time']]];
-        }
-        // Set field_difficulty Field.
-        if (!empty($data['difficulty'])) {
-          $values['field_difficulty'] = $data['difficulty'];
-        }
-        // Set field_number_of_servings Field.
-        if (!empty($data['number_of_servings'])) {
-          $values['field_number_of_servings'] = [['value' => $data['number_of_servings']]];
-        }
-        // Set field_ingredients Field.
-        if (!empty($data['ingredients'])) {
-          $ingredients = explode(',', $data['ingredients']);
-          $values['field_ingredients'] = [];
-          foreach ($ingredients as $ingredient) {
-            $values['field_ingredients'][] = ['value' => $ingredient];
-          }
-        }
-        // Set field_recipe_instruction Field.
-        if (!empty($data['recipe_instruction'])) {
-          $recipe_instruction_path = $module_path . '/default_content/recipe_instructions/' . $data['recipe_instruction'];
-          $recipe_instructions = file_get_contents($recipe_instruction_path);
-          if ($recipe_instructions !== FALSE) {
-            $values['field_recipe_instruction'] = [['value' => $recipe_instructions, 'format' => 'basic_html']];
-          }
-        }
-        // Set field_tags if exists.
-        if (!empty($data['tags'])) {
-          $values['field_tags'] = [];
-          $tags = array_filter(explode(',', $data['tags']));
-          foreach ($tags as $term) {
-            $values['field_tags'][] = ['target_id' => $this->getTerm($term)];
-          }
-        }
-
-        $node = $this->entityTypeManager->getStorage('node')->create($values);
-        $node->save();
-        $uuids[$node->uuid()] = 'node';
-      }
-      $this->storeCreatedContentUuids($uuids);
-      fclose($handle);
-    }
-    return $this;
   }
 
   /**
@@ -284,6 +92,7 @@ class InstallHelper implements ContainerInjectionInterface {
    * @return $this
    */
   protected function importPages() {
+    /*
     if (($handle = fopen($this->moduleHandler->getModule('fflch_fakecontent')->getPath() . '/default_content/pages.csv', "r")) !== FALSE) {
       $headers = fgetcsv($handle);
       $uuids = [];
@@ -319,6 +128,7 @@ class InstallHelper implements ContainerInjectionInterface {
       fclose($handle);
     }
     return $this;
+    */
   }
 
   /**
@@ -327,6 +137,7 @@ class InstallHelper implements ContainerInjectionInterface {
    * @return $this
    */
   protected function importBlockContent() {
+    /*
     $module_path = $this->moduleHandler->getModule('fflch_fakecontent')->getPath();
     $block_content_entities = [
       'umami_home_banner' => [
@@ -420,6 +231,7 @@ class InstallHelper implements ContainerInjectionInterface {
       $this->storeCreatedContentUuids([$block_content->uuid() => 'block_content']);
     }
     return $this;
+    */
   }
 
   /**
@@ -428,6 +240,7 @@ class InstallHelper implements ContainerInjectionInterface {
    * @return $this
    */
   public function deleteImportedContent() {
+    /*
     $uuids = $this->state->get('fflch_fakecontent_uuids', []);
     $by_entity_type = array_reduce(array_keys($uuids), function ($carry, $uuid) use ($uuids) {
       $entity_type_id = $uuids[$uuid];
@@ -440,68 +253,9 @@ class InstallHelper implements ContainerInjectionInterface {
       $storage->delete($entities);
     }
     return $this;
+    */
   }
 
-  /**
-   * Looks up a user by name, if it is missing the user is created.
-   *
-   * @param string $name
-   *   Username.
-   *
-   * @return int
-   *   User ID.
-   */
-  protected function getUser($name) {
-    $user_storage = $this->entityTypeManager->getStorage('user');
-    $users = $user_storage->loadByProperties(['name' => $name]);;
-    if (empty($users)) {
-      // Creating user without any password.
-      $user = $user_storage->create([
-        'name' => $name,
-        'status' => 1,
-        'roles' => ['author'],
-        'mail' => mb_strtolower(str_replace(' ', '.', $name)) . '@example.com',
-      ]);
-      $user->enforceIsNew();
-      $user->save();
-      $this->storeCreatedContentUuids([$user->uuid() => 'user']);
-      return $user->id();
-    }
-    $user = reset($users);
-    return $user->id();
-  }
-
-  /**
-   * Looks up a term by name, if it is missing the term is created.
-   *
-   * @param string $term_name
-   *   Term name.
-   * @param string $vocabulary_id
-   *   Vocabulary ID.
-   *
-   * @return int
-   *   Term ID.
-   */
-  protected function getTerm($term_name, $vocabulary_id = 'tags') {
-    $term_name = trim($term_name);
-    $term_storage = $this->entityTypeManager->getStorage('taxonomy_term');
-    $terms = $term_storage->loadByProperties([
-      'name' => $term_name,
-      'vid' => $vocabulary_id,
-    ]);
-    if (!$terms) {
-      $term = $term_storage->create([
-        'name' => $term_name,
-        'vid' => $vocabulary_id,
-        'path' => ['alias' => '/' . Html::getClass($vocabulary_id) . '/' . Html::getClass($term_name)],
-      ]);
-      $term->save();
-      $this->storeCreatedContentUuids([$term->uuid() => 'taxonomy_term']);
-      return $term->id();
-    }
-    $term = reset($terms);
-    return $term->id();
-  }
 
   /**
    * Creates a file entity based on an image path.
@@ -513,6 +267,7 @@ class InstallHelper implements ContainerInjectionInterface {
    *   File ID.
    */
   protected function createFileEntity($path) {
+    /*
     $uri = $this->fileUnmanagedCopy($path);
     $file = $this->entityTypeManager->getStorage('file')->create([
       'uri' => $uri,
@@ -521,6 +276,7 @@ class InstallHelper implements ContainerInjectionInterface {
     $file->save();
     $this->storeCreatedContentUuids([$file->uuid() => 'file']);
     return $file->id();
+    */
   }
 
   /**
@@ -531,8 +287,10 @@ class InstallHelper implements ContainerInjectionInterface {
    *   type.
    */
   protected function storeCreatedContentUuids(array $uuids) {
+    /*
     $uuids = $this->state->get('fflch_fakecontent_uuids', []) + $uuids;
     $this->state->set('fflch_fakecontent_uuids', $uuids);
+    */
   }
 
   /**
@@ -545,8 +303,10 @@ class InstallHelper implements ContainerInjectionInterface {
    *   The path to the new file, or FALSE in the event of an error.
    */
   protected function fileUnmanagedCopy($path) {
+    /*
     $filename = basename($path);
     return file_unmanaged_copy($path, 'public://' . $filename, FILE_EXISTS_REPLACE);
+    */
   }
 
 }
